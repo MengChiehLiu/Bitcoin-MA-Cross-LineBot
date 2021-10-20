d9e1301852f8a50f4f4e134d9c1a1afc31494bb0
Meng-Chieh, Liu <77425545+MengChiehLiu@users.noreply.github.com>
2021-10-21 01:16:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,46 +25,5 @@ def callback():
 
         return "OK"
 
-@app.route("/function")
-def function():
-    # get price from binance api
-    client = Client()
-    klines = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_4HOUR, "two week ago")
-
-    # function to calculate sma value
-    def sma(n):
-        return sum([float(k[4]) for k in klines[-n-1:-1]])/n
-
-    # function to calculate previous sma value
-    def prev_sma(n):
-        return sum([float(k[4]) for k in klines[-n-2:-2]])/n
-
-    # calculate sma 5 and 60
-    sma60 = sma(60)
-    sma5 = sma(5)
-
-    # calculate previous sma 5 and 60
-    psma60 = prev_sma(60)
-    psma5 = prev_sma(5)
-
-    # golden cross
-    if sma5 > sma60 and psma5 < psma60:
-        line_bot_api.push_message("U55cce311c3805b9fa42f53867bd5d88d", TextSendMessage(text='4hr flip to long'))
-        return '4hr flip to long'
-        
-    # dead cross
-    if sma5 < sma60 and psma5 > psma60:
-        line_bot_api.push_message("U55cce311c3805b9fa42f53867bd5d88d", TextSendMessage(text='4hr flip to short'))
-        return '4hr flip to short'
-
-    # hold for short
-    if sma5 < sma60 and psma5 < psma60:
-        return 'hold short'
-
-    # hold for long
-    if sma5 > sma60 and psma5 > psma60:
-        return 'hold long'
-
-
 if __name__ == "__main__":
     app.run()
